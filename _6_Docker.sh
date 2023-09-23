cede81ed1605781047aa0e74eab811c832d374ca
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-23 22:35:57 +0300

    docker revize

--- _6_Docker.sh
+++ _6_Docker.sh
@@ -9,7 +9,8 @@ DELETED="Silme"
 WHICH="Nerede ?"
 COMMONINFORMATION=" Genel Bilgiler"
 LOGIN="Docker Login"
-DOKCER_PULL="Docker Pulling"
+LOGOUT="Docker Logout"
+DOCKER_PULL="Docker Pulling"
 
 # System Variable
 
@@ -87,9 +88,9 @@ then
      sudo apt-get update
      sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-     sudo systemctl status docker
-     sleep 2
-     q
+     # sudo systemctl status docker
+     # sleep 2
+     # q
      sudo systemctl enable --now docker 
      sudo systemctl start docker
      # sudo systemctl status docker
@@ -110,38 +111,41 @@ else
 fi 
 
 
+
+
 # Docker Login
 sleep 2
 echo -e "\n### ${LOGIN} ###"
-echo -e "\nDockerHub'a Giriş yapmak istiyor musunuz ? E/H " loginResult
-if [[ $loginResult == "E" || $loginResult == "e"  ]]
+read -p "\nDockerHub'a Giriş yapmak istiyor musunuz ? E/H? " updatedResult
+if [[ $updatedResult == "E" || $updatedResult == "e"  ]]
 then
     echo -e "Docker Login ... "  
     sudo docker login
 else
-     echo -e "Docker Login yapılmadı Yapılmadı!!!\n "   
+     echo -e "apt-get Update List Güncelleme Yapılmadı!!!\n "   
 fi
+
 
 # Docker Logout
 sleep 2
 echo -e "\n### ${LOGOUT} ###"
-echo -e "\nDockerHub'a Çıkış yapmak istiyor musunuz ? E/H " logoutResult
-if [[ $logoutResult == "E" || $logoutResult == "e"  ]]
+read -p "\nDockerHub'a Çıkış yapmak istiyor musunuz ? E/H? " updatedResult
+if [[ $updatedResult == "E" || $updatedResult == "e"  ]]
 then
-    echo -e "Docker Logout ... "  
+    echo -e "Docker Login ... "  
     sudo docker logout
 else
-     echo -e "Docker Logout yapılmadı Yapılmadı!!!\n "   
+     echo -e "apt-get Update List Güncelleme Yapılmadı!!!\n "   
 fi
 
 
 # Docker Pull
 sleep 2
-echo -e "\n### ${DOKCER_PULL} ###"
-echo -e "\nDockerHub'a Pull yapmak istiyor musunuz ? E/H " pullingResult
-if [[ $pullingResult == "E" || $pullingResult == "e"  ]]
+echo -e "\n### ${DOCKER_PULL} ###"
+read -p "\nDockerHub'a Pull  yapmak istiyor musunuz ? E/H? " updatedResult
+if [[ $updatedResult == "E" || $updatedResult == "e"  ]]
 then
-    echo -e "Docker Pulling ... "  
+       echo -e "Docker Pulling ... "  
     sudo docker pull nginx
     sudo docker pull httpd
     sudo docker pull mysql
@@ -149,22 +153,7 @@ then
     sudo docker pull ubuntu
     sudo docker pull centos
 else
-     echo -e "Docker Pulling yapılmadı Yapılmadı!!!\n "   
-fi
-
-
-# SİLMEK
-sleep 2
-echo -e "\n### ${DELETED} ###"
-echo -e "\nMAven Dosyasını Silmek istiyor musunuz ? E/H " deletedResult
-if [[ $deletedResult == "E" || $deletedResult == "e"  ]]
-then
-    echo -e "Temizlik Başlandı... "
-    sudo rm -f apache-maven-3.6.3-bin.tar.gz
-    sudo apt-get clean
-    sudo apt-get autoremove -y
-else
-     echo -e "Temizlik Yapılmadı!!!\n "   
+     echo -e "apt-get Update List Güncelleme Yapılmadı!!!\n "   
 fi
 
 # Temizlik
